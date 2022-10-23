fcc603ce95919abee0104fb90bb29666607acc45
iseneld <84731866+iseneld@users.noreply.github.com>
2022-10-23 21:22:57 +0200

    Project reset.

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full h-16 p-8 flex items-center bg-zinc-900">
+      Footer
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="w-full h-16 p-8 flex items-center bg-zinc-900">
+      Header
+    </header>
+  );
+};
+
+export default Header;
